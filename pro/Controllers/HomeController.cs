84b5ce6d0bb3eddf516e2e8b045fb6a438ca2ab1
dang5544 <160257399+dang5544@users.noreply.github.com>
2026-05-17 13:44:36 -0600

    Add project files.

---
+++ pro/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Hàm này để sau này gọi trang Login (Views/Home/Login.cshtml)
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Hàm này để gọi trang Mentor (Views/Home/Page2.cshtml)
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        // Hàm này để gọi trang Mentor (Views/Home/Page2.cshtml)
+        public ActionResult Page2()
+        {
+            return View();
+        } // Hàm này để gọi trang Mentor (Views/Home/Page2.cshtml)
+        public ActionResult Page3()
+        {
+            return View();
+        }
+    }
+}
---
+++ pro/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Http;
+
+namespace pro.Controllers
+{
+    // 1. Định nghĩa cấu trúc dữ liệu JSON gửi lên (Model)
+    public class ActivityData
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string DateTime { get; set; }
+        public string Duration { get; set; }
+        public string Desc { get; set; }
+        public string Comment { get; set; }
+    }
+
+    // 2. Class API Controller duy nhất để xử lý luồng dữ liệu
+    public class UserController : ApiController
+    {
+        // Hàm POST để nhận dữ liệu dạng JSON từ giao diện gửi lên
+        // Đường dẫn gọi API này sẽ là: /api/activity/save
+        [HttpPost]
+        [Route("api/activity/save")]
+        public IHttpActionResult SaveActivity([FromBody] ActivityData incomingModel)
+        {
+            if (incomingModel == null)
+            {
+                return BadRequest("Dữ liệu gửi lên trống hoặc sai định dạng!");
+            }
+
+            try
+            {
+                // KHU VỰC XỬ LÝ: Sau này nhóm bạn sẽ viết code kết nối Database (SQL Server) 
+                // để lưu biến `incomingModel` này vào bảng dữ liệu tại đây.
+
+                // Hiện tại, trả về một chuỗi JSON báo thành công cho giao diện biết
+                return Ok(new
+                {
+                    success = true,
+                    message = $"[API Server] Đã nhận thành công hoạt động: {incomingModel.Name}!"
+                });
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
